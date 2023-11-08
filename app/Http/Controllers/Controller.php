2d47305289066c8380e47c5f53da78be0b537539
Chhounseangly <91061974+Chhounseangly@users.noreply.github.com>
2023-11-08 18:31:16 +0700

    add more category and controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,23 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+        //respone success
+        public function Success(mixed $data, string $message, int $statusCode)
+        {
+            return response()->json([
+                'message'  => $message,
+                'data' => $data
+            ], $statusCode);
+        }
+    
+        //respone fails
+        public function Fails(mixed $data, string $message, int $statusCode)
+        {
+            return response()->json([
+                'message'  => $message,
+                'data' => $data
+            ], $statusCode);
+        }
+    
 }
