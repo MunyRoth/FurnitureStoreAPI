4ad5faa35777805a3ae4cf1e39ff38ced17dfd8b
មុនី​ រតន៍​ <dr.munyroth@gmail.com>
2023-11-16 16:29:21 +0700

    improve on get all products with isFavorite

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,6 +8,7 @@ use App\Models\Image;
 use App\Models\Product;
 use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
@@ -21,12 +22,20 @@ class ProductController extends Controller
      */
     public function index(Product $products)
     {
+        // Get the authenticated user, if any
+        $user = auth()->user();
+
+        // Determine the condition for the isFavorite column
+        $isFavoriteCondition = $user ? 'IF(favourites.user_id = ' . $user->id . ' OR favourites.product_id IS NOT NULL, 1, 0)' : '0';
+
+        // Fetch data from the products table, including the isFavorite column
         $data = $products
-            ->select('products.id', 'products.name', 'products.price', 'products.imageUrl', 'favourites.is_favourited as isFavorite')
+            ->select('products.id', 'products.name', 'products.price', 'products.imageUrl', DB::raw($isFavoriteCondition . ' as isFavorite'))
             ->leftJoin('favourites', 'products.id', '=', 'favourites.product_id')
             ->get();
-    
-        return $this->Res($data, "got data successfully", 200);
+
+         // Return the response with the fetched data
+         return $this->Res($data, 'got data successfully', 200);
     }
 
     /**
@@ -34,19 +43,23 @@ class ProductController extends Controller
      */
     public function store(StoreProductRequest $request, Product $product)
     {
+        // Upload the image to Cloudinary
         $imageUrl = Cloudinary::upload($request->file('image')->getRealPath(), [
             'folder' => 'FurnitureStore'
         ])->getSecurePath();
 
+        // Fill the product model with the request data
         $product->fill($request->only(['name', 'price', 'category_id', 'description']));
         $product->imageUrl = $imageUrl;
         $product->save();
 
+        // Create and save a new image record associated with the product
         $image = new Image;
         $image->product_id = $product->id;
         $image->imageUrl = $product->imageUrl;
         $image->save();
 
+        // Return the response with the created product data
         return $this->Res($product, 'created successfully', 201);
     }
 
@@ -56,6 +69,7 @@ class ProductController extends Controller
     public function show($id)
     {
         try {
+            // Find the product by ID with its associated image URLs
             $product = Product::with('imageUrls')->findOrFail($id);
             return $this->Res($product, 'gotten successfully', 200);
         } catch (ModelNotFoundException $e) {
@@ -69,10 +83,16 @@ class ProductController extends Controller
     public function update(UpdateProductRequest $request, $id)
     {
         try {
+            // Find the product by ID
             $product = Product::findOrFail($id);
+
+            // Update the product with the specified data
             $product->update($request->only(['name', 'price', 'description']));
+
+            // Return the response with the updated product data
             return $this->Res($product, 'updated successfully', 200);
         } catch (ModelNotFoundException $e) {
+            // Return a not found response if the product is not found
             return $this->Res(null, 'not found', 404);
         }
     }
@@ -83,10 +103,14 @@ class ProductController extends Controller
     public function destroy($id)
     {
         try {
+            // Find the product by ID and delete it
             $product = Product::findOrFail($id);
             $product->delete();
+
+            // Return a success response
             return $this->Res(null, 'deleted successfully', 200);
         } catch (ModelNotFoundException $e) {
+            // Return a not found response if the product is not found
             return $this->Res(null, 'not found', 404);
         }
     }
