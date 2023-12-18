b8b82de871c2b1eebf0701d67441ff869696303b
មុនី​ រតន៍​ <dr.munyroth@gmail.com>
2023-12-18 14:08:02 +0700

    remove welcome

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Hack me if you can! 😜',
+        'data' => null
+    ]);
 });
